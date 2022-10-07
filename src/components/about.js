d033d4e79cead5140e7b9871e91478ffbe35df96
mjlindskog <lindskogmarkj@gmail.com>
2022-10-07 12:18:07 -0400

    update about section

--- src/components/about.js
+++ src/components/about.js
@@ -17,7 +17,7 @@ export default function About() {
   return (
     <Container sx={{ display: "relative", mt: 20, height: '50vh', width: '100%', justifyContent: 'center' }}>
       <Typography className="about" variant='h6' align="center" sx={{ color: "white", fontWeight: "bolder", display: "block", px: 5 }}>
-        Recent graduate of a coding boot camp through Ohio State University. Plan to utilize my new and growing skills around Full Stack Development and established communictions skills through various sales positions to become a vital member of a development team. Now possessing skills around Javascript, React, Node, HTML and CSS.
+        Currently a software quality assurance engineer with training in manual testing. My previous completion of an Ohio State University full stack web development bootcamp helped me advance my knowledge in HTML, CSS, Javascript, ReactJS, NodeJS, and SQL. I want to use my current QA skills and previous coding knowledge to further my career growth.
       </Typography>
       <Container fixed align="center" sx={{ width: '100%', justifyContent: 'center', display: 'sticky', opacity: '0.5', p: 5 }}>
         <FontAwesomeIcon icon={["fab", "html5"]} size="5x" color="black"/>
